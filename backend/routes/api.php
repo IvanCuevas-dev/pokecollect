56625199fc04ca617a95713fb2a44a598510e10a
IvanCuevas-dev <csalgueroivan@gmail.com>
2026-03-31 11:10:35 +0200

    Añadido ruta comprar sobre

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,12 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PokemonController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\ShopController;
 
 
-Route::get('/pokemon', [PokemonController::class, 'index']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return response()->json($request->user());
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+    Route::post('/buy', [ShopController::class, 'buy']);
+    Route::get('/pokemon', [PokemonController::class, 'index']);
 });
